67389c74a03fe4d774c69a26262221dc3957a3d2
SA Tahaa <satahaa12345@gmail.com>
2025-02-27 03:00:41 +0600

    Kinda generated something?

--- src/main.c
+++ src/main.c
@@ -1,38 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
+typedef enum {
+    INT,
+} TokenLiteral;
 
-int main(const int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("No file has been provided\n");
-        exit(EXIT_FAILURE);
-    }
-    char* path = argv[1];
-    const size_t len = strlen(path);
-    if (path[len - 1] != 'r' || path[len - 2] != 'o' || path[len - 3] != 'c' || path[len - 4] != '.' || len < 4) {
-        printf("Invalid filetype: %s, file has to end with .cor extension", path);
-        exit(EXIT_FAILURE);
+typedef enum {
+    LET, RET
+} TokenKeyword;
+
+typedef enum {
+    SEMI, OPEN, CLOSE,
+} TokenDelimiter;
+
+typedef struct {
+    union {
+        TokenLiteral ltype;
+        TokenDelimiter dtype;
+        TokenKeyword ktype;
+    } type;
+    int val;
+} Token;
+
+Token *num_gen(char c, FILE *file) {
+    Token *token = malloc(sizeof(Token));
+    token->type.ltype = INT;
+
+    char word[20];
+    int i = 0;
+
+    word[i++] = c;
+    c = (char)fgetc(file);
+    while (isdigit(c) && i < 99) {
+        word[i++] = c;
+        c = (char)fgetc(file);
+        if (!isdigit(c)) {
+            ungetc(c, file);
+            break;
+        }
     }
 
-    FILE *file = fopen(path, "r");
-    if (file == NULL) {
-        printf("File not found: %s\n", path);
-        exit(EXIT_FAILURE);
+    word[i] = '\0';
+    token->val = atoi(word);
+
+    return token;
+}
+Token *keyword_gen(char c, FILE *file) {
+    Token *token = malloc(sizeof(Token));
+    char word[10];
+    int i = 0;
+
+    word[i++] = c;
+    c = (char)fgetc(file);
+    while (isalpha(c) && i < 99) {
+        word[i++] = c;
+        c = (char)fgetc(file);
+        if (!isalpha(c)) {
+            ungetc(c, file);
+            break;
+        }
     }
 
-    int cur = fgetc(file);
-    while (cur != EOF) {
-        printf("%c", cur);
-        cur = fgetc(file);
+    word[i] = '\0';
+    if (!strcmp(word, "let")) {
+        token->type.ktype = LET;
+    } else if (!strcmp(word, "ret")) {
+        token->type.ktype = RET;
     }
 
-    if (argc > 2) {
-        printf("%s.exe\n", argv[2]);
-    }
-    else {
-        printf("a.out\n");
-    }
+    return token;
+}
 
-    return 0;
+void lex(FILE *file) {
+    char c  = fgetc(file);
+    while (c != EOF) {
+        while (c == ' ' || c == '\n')
+            c = (char)fgetc(file);
+
+        if (isdigit(c)) { //number
+            Token *t = num_gen(c, file);
+            printf("LIT: %d\n", t->val);
+            free(t);
+        } else if (isalpha(c)){ //keyword
+            Token *t = keyword_gen(c, file);
+            if (t->type.ktype == LET) {
+                c = (char)fgetc(file);
+                printf("LET\n %c\n", c);
+            } else if (t->type.ktype == RET) {
+                printf("RETURN\n");
+            }
+            free(t);
+        }
+        else printf("%c\n", c); //semi
+
+        c = (char)fgetc(file);
+    }
+}
+int main() {
+    FILE *file;
+    file = fopen("../tests/test.cor", "r");
+    lex(file);
+    fclose(file);
 }
 
